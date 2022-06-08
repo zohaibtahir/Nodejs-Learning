bd5af4880883d745d4242b91e11c2a089c95c1e5
Zohaib Tahir <Zohaibtahiruaf@gmail.com>
2022-06-08 12:37:03 +0500

    learn how to connect MongoDB
    post and get data

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const express = require('express');
+const { result } = require('lodash');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// database connection string of MongoDB
+const dbURI = 'mongodb+srv://zohaib:zohaib@nodelearning.ruf1z.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+.then((result)=>app.listen(3000))
+.catch((err) => console.log(err));
+
 // view engine
 app.set('view engine' , 'ejs');
 
-// listen 
-app.listen(3000);
 
 // Middleware for static files
 app.use(express.static('public'));
+
+// add blog
+app.get('/add-blog', (req,res)=>{
+    const blog = new Blog({
+        title:'Blog title 2',
+        sinppet: 'Blog snippet will be here',
+        body: 'blog full body text will be here'
+    });
+
+    blog.save()
+    .then((result)=>{
+        res.send(result);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+});
+
+// get blogs
+app.get('/all-blogs',(req,res)=>{
+    Blog.find()
+    .then((result)=>{
+        res.send(result);
+    })
+    .catch((err)=>console.log(err));
+});
 
 // routing
 app.get('/',(req,res)=>{
